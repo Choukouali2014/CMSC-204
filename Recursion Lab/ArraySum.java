0e2c95206f26bd5103c4f89e3a1fee54f09b58bd
Frank Choukouali <fchokouali2014@gmail.com>
2021-02-22 13:14:53 -0500

    Recursion Lab

---
+++ Recursion Lab/ArraySum.java
@@ -0,0 +1,14 @@
+
+/**
+ * @author Frank Choukouali
+ *
+ */
+public class ArraySum {
+
+	public int sumOfArray(Integer[] a, int index) {
+		if(index<0)
+			return 0;
+		return a[index]+ sumOfArray(a, --index);
+	}
+
+}
